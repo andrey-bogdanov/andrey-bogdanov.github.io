03c4106a7ca4b90746073312fce97af765d19acd
Andrey <asbogdanov13@gmail.com>
2022-03-07 23:41:13 +0500

    ver 1.0.6 1.0.5 maked build

--- example/src/App.js
+++ example/src/App.js
@@ -1,76 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import { Tree } from "tree-graph-flex"
-import MainPage from './main.page';
+// import { Tree } from "tree-graph-flex"
+import MainPage from './mainpage';
 
-const data = {
-  id: "0",
-  subject: "animals",
-  children: [
-    {
-      id: "1",
-      subject: "dogs",
-      children: [
-        {
-          id: "1.1",
-          subject: "terriers",
-          children: [],
-        },
-        {
-          id: "1.2",
-          subject: "mastiffs",
-          children: [],
-        },
-      ],
-    },
-    {
-      id: "2",
-      subject: "birds",
-      children: [
-        {
-          id: "2.1",
-          subject: "parrots",
-          children: [],
-        },
-        {
-          id: "2.2",
-          subject: "crows",
-          children: [],
-        },
-      ],
-    },
-  ],
-};
-
-function node(node) { 
-  return (
-    <div> 
-      {node.subject}
-    </div>
-  )
-};
-
-function xOffset(level) {
-  const offset = 30 + level*10
-return offset
-};
 
 function App() {
-  return (
-    <div>
-      <MainPage/>
-      <div className="App">
-        <Tree data={data}
-        nodeContent={node}
-        yOffset={50}
-        xOffset={xOffset}
-        nodeWidth={150}
-        nodeHeight={50}
-        pathShape={"bezier"}
-      />     
-      </div>
-    </div>
-  );
+  return (     
+  <div className="App">
+    <MainPage/>
+  </div> )
 }
 
 export default App;
---
+++ example/src/mainpage.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import { Tree } from "tree-graph-flex";
+import "./mainpage.css"
+
+const data = {
+  id: "0",
+  subject: "animals",
+  children: [
+    {
+      id: "1",
+      subject: "dogs",
+      children: [
+        {
+          id: "1.1",
+          subject: "terriers",
+          children: [],
+        },
+        {
+          id: "1.2",
+          subject: "mastiffs",
+          children: [],
+        },
+      ],
+    },
+    {
+      id: "2",
+      subject: "birds",
+      children: [
+        {
+          id: "2.1",
+          subject: "parrots",
+          children: [],
+        },
+        {
+          id: "2.2",
+          subject: "crows",
+          children: [],
+        },
+      ],
+    },
+  ],
+};
+
+function node(node) { 
+  return (
+    <div> 
+      {node.subject}
+    </div>
+  )
+};
+
+function xOffset(level) {
+  const offset = 30 + level * 10
+  return offset
+};
+
+class MainPage extends Component {
+  render() {
+    return (
+      <div className="main-page">
+        <div className="tree-container">
+          <Tree data={data}
+          nodeContent={node}
+          yOffset={50}
+          xOffset={xOffset}
+          nodeWidth={150}
+          nodeHeight={50}
+          pathShape={"bezier"}
+          />
+        </div>  
+      </div>
+    );
+  }
+}
+
+export default MainPage
