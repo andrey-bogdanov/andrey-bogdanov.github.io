f428c9f7ac294e3514c373ba27acb781e8c758f6
Andrey <asbogdanov13@gmail.com>
2022-03-09 23:42:16 +0500

    added interactivity in example ver 1.0.0

--- example/src/mainpage.js
+++ example/src/mainpage.js
@@ -12,12 +12,39 @@ function node(node) {
   )
 };
 
-function xOffset(level) {
-  const offset = 30 + level * 10
-  return offset
-};
+// function xOffsetFunction(level) {
+//   const offset = 30 + level * 10
+//   return offset
+// };
 
 class MainPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      yOffset: 20,
+      xOffset: 50,
+      nodeHeight: 30,
+      nodeWidth: 100,
+      pathShape: "bezier"
+    }
+  }
+
+  yOffsetChanger (yOffset) {
+    this.setState({yOffset: yOffset})
+  }
+
+  xOffsetChanger (xOffset) {
+    this.setState({xOffset: xOffset})
+  }
+
+  nodeWidthChanger (nodeWidth) {
+    this.setState({nodeWidth: nodeWidth})
+  }
+
+  nodeHeightChanger (nodeHeight) {
+    this.setState({nodeHeight: nodeHeight})
+  }
+
   render() {
     return (
       <div className="main-page">
@@ -31,36 +58,34 @@ class MainPage extends Component {
         </div>
 
         <div className="props-container content-container">
+
+          <p className="container-header">Code</p>
+            <br></br>
           <p className="container-header">Properties</p>
-          <form className="props-form">
-            <p className="fild-name">data</p>
-            <input name="data" type="text" placeholder="data" size="26"/>
-            <p className="fild-name">nodeContent</p>
-            <input name="nodeContent" type="text" placeholder="node => <div> {node.subject}" size="26"/>
+          <div className="props-form">
             <p className="fild-name">yOffset</p>
-            <input name="yOffset" type="text" placeholder="50" size="26"/>
+            <input name="yOffset" type="number" value={this.state.yOffset} className="input-prop"  onChange={event => this.yOffsetChanger(event.target.value)}/>
             <p className="fild-name">xOffset</p>
-            <input name="xOffset" type="text" placeholder="level => 30 + level * 10" size="26"/> 
+            <input name="xOffset" type="number" value={this.state.xOffset} className="input-prop"  onChange={event => this.xOffsetChanger(event.target.value)}/> 
             <p className="fild-name">nodeWidth</p>
-            <input name="nodeWidth" type="text" placeholder="100" size="26" />
+            <input name="nodeWidth" type="number" value={this.state.nodeWidth} className="input-prop" onChange={event => this.nodeWidthChanger(event.target.value)}/>
             <p className="fild-name">nodeHeight</p>
-            <input name="nodeHeight" type="text" placeholder="25" size="26"/>
+            <input name="nodeHeight" type="number" value={this.state.nodeHeight} className="input-prop" onChange={event => this.nodeHeightChanger(event.target.value)}/>
             <p className="fild-name">pathShape</p>
-            <input name="pathShape=" type="text" placeholder="'bezier'" size="26"/>
-            <p className="fild-name">lineClassName</p>
-            <input name="lineClassName=" type="text" placeholder="connectingLine" size="26"/>
-            <input type="submit" className="submit-bottom"></input>
-          </form>
+            <input name="pathShape=" type="text" value={this.state.pathShape} className="input-prop input-path-shape"/>
+          </div>
         </div>
 
         <div className="tree-container content-container">
-          <Tree data={data}
-          nodeContent={node}
-          yOffset={50}
-          xOffset={xOffset}
-          nodeWidth={100}
-          nodeHeight={25}
-          pathShape={"bezier"}
+          <Tree 
+          
+            data={data}
+            nodeContent={node}
+            yOffset={Number(this.state.yOffset)}
+            xOffset={Number(this.state.xOffset)}
+            nodeWidth={Number(this.state.nodeWidth)}
+            nodeHeight={Number(this.state.nodeHeight)}
+            pathShape={this.state.pathShape}
           />
         </div>
       </div>
